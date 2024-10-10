588da45e219222683c1340afd35836123196c1cd
multiplesplit <contactcatevari@gmail.com>
2024-10-10 13:16:03 +0300

    product_page

---
+++ portfolio/product_page/dist/js/swipers.js
@@ -0,0 +1,28 @@
+const reviewsSlider = new Swiper(".reviews__swiper", {
+  speed: 400,
+  slidesPerView: 1.1,
+  spaceBetween: 10,
+  grabCursor: true,
+  updateOnWindowResize: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+    1201: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 24,
+    },
+    490: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 12,
+    },
+  },
+});
+
+reviewsSlider.on("slideChange", function () {
+  document.querySelector(".reviews__counter span").textContent =
+    reviewsSlider.activeIndex + 1;
+});
---
+++ portfolio/product_page/resources/js/main.js
@@ -0,0 +1,3 @@
+if (document.querySelector(".swiper")) {
+  import("./swipers.js");
+}
